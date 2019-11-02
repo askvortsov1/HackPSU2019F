26fd4afa2a962d70fc97e8357734a01b5323eb40
Alexander Skvortsov <askvortsov@youthneuro.org>
2019-11-02 19:30:58 -0400

    Added collectstatic support

--- gunicorn.sh
+++ gunicorn.sh
@@ -5,5 +5,6 @@ sleep 4
 python3 manage.py runserver 0.0.0.0:80 &
 python3 manage.py makemigrations
 python3 manage.py migrate
+python3 manage.py collectstatic --noinput --verbosity 0
 echo "Server Successfully Started"
 sleep infinity
--- src/core/settings.py
+++ src/core/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+PROJECT_DIR = os.path.dirname(__file__)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
@@ -158,3 +159,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static/')
