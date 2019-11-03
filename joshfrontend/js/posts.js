1922d89ad7076b62eee920c5983a64f30359cbac
J. Famous <joshua.famous@gmail.com>
2019-11-03 11:18:56 -0500

    Working login

--- joshfrontend/js/posts.js
+++ joshfrontend/js/posts.js
@@ -1,17 +1,48 @@
 function submitLogin(){
     var loginData =
-        {
-         email: document.getElementById('loginFormEmail').value,
-         password: document.getElementById('loginFormPassword').value
-        };
-
-    var loginDataString = JSON.stringify(loginData);
+        {email: document.getElementById('loginFormEmail').value,password:document.getElementById('loginFormPassword').value};
 
     $.ajax({
         type: "POST",
         url: "http://hackpsu-fall2019.herokuapp.com/rest-auth/login/",
         dataType: "json",
-        data: loginDataString,
+        data: loginData,
+        success: function (data) {
+            if(data){
+                console.log("Got token " + data[key]);
+                document.location.href = document.location.href.replace('index.html','') + "pages/main.html";
+            }
+            else{
+                console.log("Failed user authentication");
+                document.getElementById('loginFailText').style.display = 'block';
+            }
+            console.log("Return data was : " + data);
+        },
+        error: function () {
+            alert('Error');
+        }
+    });
+}
+
+function submitRegister(){
+    var registerData =
+        {
+            email:document.getElementById('registerFormEmail').value,
+            password1:document.getElementById('registerFormPassword').value,
+            password2:document.getElementById('registerFormPassword2').value,
+            fname:document.getElementById('registerFormFirstname').value,
+            lname:document.getElementById('registerFormLastname').value,
+            birthday:document.getElementById('registerFormBirthday').value,
+            gender:document.getElementById('registerFormGender').value
+        };
+
+    var registerStringData = JSON.stringify(registerData);
+
+    $.ajax({
+        type: "POST",
+        url: "http://hackpsu-fall2019.herokuapp.com/rest-auth/registration/",
+        dataType: "json",
+        data: registerStringData,
         success: function (data) {
             if(data){
                 console.log("Got token " + data[key]);
