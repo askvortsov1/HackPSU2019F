6892dcb8f6c0e3a1bd04a7845b0ffff4ad878c71
Matthew So <matthewclso@gmail.com>
2019-11-03 02:25:59 -0500

    no message

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -441,6 +441,9 @@ class MyCustomFormStatePrimaryRequest extends State<MyCustomFormPrimaryRequest> 
   final severityController = TextEditingController();
   final itemController = TextEditingController();
 
+  int severity;
+  String item;
+
   @override
   void dispose() {
     // Clean up the controller when the widget is disposed.
@@ -454,74 +457,98 @@ class MyCustomFormStatePrimaryRequest extends State<MyCustomFormPrimaryRequest> 
   @override
   Widget build(BuildContext context) {
     // Build a Form widget using the _formKey created above.
-    return Form(
-      key: _formKey,
-      child: ListView(
-        children: <Widget>[
-          Padding(
-              child: Text('Level of Severity', textAlign: TextAlign.center, textScaleFactor: 2),
-              padding: const EdgeInsets.symmetric(vertical: 16.0)
-          ),
-          TextFormField(
-            controller: severityController,
-            validator: (value) {
-              if (value.isEmpty) {
-                return 'Please enter  some text';
-              }
-              return null;
-            },
-          ),
-          Padding(
-              padding: const EdgeInsets.symmetric(vertical: 8.0)
-          ),
-          Text('Item', textAlign: TextAlign.center, textScaleFactor: 2,),
-          TextFormField(
-            controller: itemController,
-            validator: (value) {
-              if (value.isEmpty) {
-                return 'Please enter some text';
-              }
-              return null;
-            },
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 16.0),
-            child: RaisedButton(
-              onPressed: () {
-                // Validate returns true if the form is valid, or false
-                // otherwise.
-                if (_formKey.currentState.validate()) {
-                  _makePostRequest() async {
-                    // set up POST request arguments
-                    String url = 'https://hackpsu-fall2019.herokuapp.com/rest-auth/login/';
-                    Map<String, String> headers = {"Content-type": "application/json"};
-                    String email = emailController.text;
-                    String password = passwordController.text;
-                    String severity = severityController.text;
-                    String item = itemController.text;
-                    print(email);
-                    print(password);
-                    String json = '{"email": "$email", "password": "$password", "severity" : "$severity", "item" : "$item"}';
-                    Response response = await post(url, headers: headers, body: json);
-                    int statusCode = response.statusCode;
-                    String body = response.body;
-                    print(body);
-                    print(statusCode);
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => RequestDetails()));
-                  }
-                  Scaffold.of(context)
-                      .showSnackBar(SnackBar(content: Text('Processing Data')));
-                  _makePostRequest();
-                }
-              },
-              child: Text('Submit'),
-            ),
-          ),
-        ],
+    return MaterialApp(
+      title: 'Request Page',
+      home: Scaffold(
+          body: ListView(
+            children: ListTile.divideTiles(
+              context: context,
+              tiles: [
+
+//                Container(
+//                    color: Colors.grey[200],
+//                    child: ListTile(
+//                        title: Center(
+//                          child: Text('Go Back', style: TextStyle(fontWeight: FontWeight.bold)),
+//                        ),
+//                        onTap: (){
+//                          Navigator.pop(context);
+//                        }
+//                    )
+//                ),
+
+                Column(
+                  children: <Widget>[
+                    ListTile(
+                      title: Text('Severity of Situation', textScaleFactor: 1.1, textAlign: TextAlign.center,),
+                    ),
+                    CheckboxListTile(
+//                      onChanged: (bool checked) {
+//                        severity = 3;
+//                    },
+                      title: Text('High'),
+
+                    ),
+                    ListTile(
+                      title: Text('Medium'),
+                    ),
+                    ListTile(
+                      title: Text('Low'),
+                    ),
+                  ],
+                ),
+                Column(
+                  children: <Widget>[
+                    ListTile(
+                      title: Text('Items', textScaleFactor: 1.1, textAlign: TextAlign.center,),
+                    ),
+                    ListTile(
+                      title: Text('Food/water'),
+                    ),
+                    ListTile(
+                      title: Text('Clothing'),
+                    ),
+                    ListTile(
+                      title: Text('Shelter'),
+                    ),
+                    ListTile(
+                      title: Text('Toiletries'),
+                    ),
+                    Container(
+                        color: Colors.grey[200],
+                        child: ListTile(
+                          title: Center(
+                            child: Text('Next', style: TextStyle(fontWeight: FontWeight.bold)),
+                          ),
+                          onTap: () {
+                            //NewRequest n1 = new NewRequest()
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) => RequestDetails()),
+                            );
+
+                          },
+                        )
+                    ),
+                  ],
+                )
+              ],
+            ).toList(),
+          )
+
       ),
     );
+
+  }
+}
+class NewRequest {
+  @override
+  int severity;
+  String item;
+
+  NewRequest(int severity, String item) {
+    this.severity = severity;
+    this.item = item;
   }
 }
 
@@ -570,7 +597,7 @@ class MyCustomFormStateRequestDetails extends State<MyCustomFormRequestDetails> 
       key: _formKey,
       child: ListView(
         children: <Widget>[
-          Text('Reason for Severity', textAlign: TextAlign.center, textScaleFactor: 2),
+          Text('Situation Details', textAlign: TextAlign.center, textScaleFactor: 2),
           TextFormField(
             controller: severityDetailController,
             validator: (value) {
