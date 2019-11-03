9cf9f138d9c0a895febe7b033512d33dfc618c24
g1776 <gregoryg323@gmail.com>
2019-11-03 00:15:59 -0400

    More shit getting done

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -4,7 +4,7 @@ import 'package:http/http.dart';
 void main() {
   runApp(MaterialApp(
     title: 'I Need Help',
-    home: LoginPage(),
+    home: PrimaryRequest(),
   ));
 }
 
@@ -22,17 +22,13 @@ class RegisterPage extends StatelessWidget{
     );
   }
 }
-
 class MyCustomFormRegister extends StatefulWidget {
   @override
   MyCustomFormStateRegister createState() {
     return MyCustomFormStateRegister();
   }
 }
-
-// Create a corresponding State class.
-// This class holds data related to the form.
-class MyCustomFormStateRegister extends State<MyCustomForm> {
+class MyCustomFormStateRegister extends State<MyCustomFormRegister> {
   // Create a global key that uniquely identifies the Form widget
   // and allows validation of the form.
   //
@@ -206,16 +202,12 @@ class LoginPage extends StatelessWidget{
     );
   }
 }
-
 class MyCustomForm extends StatefulWidget {
   @override
   MyCustomFormState createState() {
     return MyCustomFormState();
   }
 }
-
-// Create a corresponding State class.
-// This class holds data related to the form.
 class MyCustomFormState extends State<MyCustomForm> {
   // Create a global key that uniquely identifies the Form widget
   // and allows validation of the form.
@@ -406,169 +398,208 @@ class LaunchPage extends StatelessWidget {
   }
 }
 
-
-// Create a Form widget.
-
-
 class PrimaryRequest extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Request Page',
-      home: Scaffold(
+        title: 'Primary Request',
+        home: Scaffold(
           appBar: AppBar(
-            title: Text('Requests'),
+            title: Text('Request Help'),
           ),
-          body: ListView(
-            children: ListTile.divideTiles(
-              context: context,
-              tiles: [
-                Container(
-                    color: Colors.grey[200],
-                    child: ListTile(
-                        title: Center(
-                          child: Text('Go Back', style: TextStyle(fontWeight: FontWeight.bold)),
-                        ),
-                        onTap: (){
-                          Navigator.pop(context);
-                        }
-                    )
-                ),
-                Column(
-                  children: <Widget>[
-                    ListTile(
-                      title: Text('Severity of Situation', textScaleFactor: 1.1, textAlign: TextAlign.center,),
-                    ),
-                    ListTile(
-                      title: Text('High'),
-                    ),
-                    ListTile(
-                      title: Text('Medium'),
-                    ),
-                    ListTile(
-                      title: Text('Low'),
-                    ),
-                  ],
-                ),
-                Column(
-                  children: <Widget>[
-                    ListTile(
-                      title: Text('Items', textScaleFactor: 1.1, textAlign: TextAlign.center,),
-                    ),
-                    ListTile(
-                      title: Text('Food/water'),
-                    ),
-                    ListTile(
-                      title: Text('Clothing'),
-                    ),
-                    ListTile(
-                      title: Text('Shelter'),
-                    ),
-                    ListTile(
-                      title: Text('Toiletries'),
-                    ),
-                    Container(
-                        color: Colors.grey[200],
-                        child: ListTile(
-                          title: Center(
-                            child: Text('Next', style: TextStyle(fontWeight: FontWeight.bold)),
-                          ),
-                          onTap: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(builder: (context) => RequestDetails()),
-                            );
-                          },
-                        )
-                    ),
-                  ],
-                )
-              ],
-            ).toList(),
-          )
+          body: MyCustomFormPrimaryRequest(),
+        )
+    );
+  }
+}
+class MyCustomFormPrimaryRequest extends StatefulWidget {
+  @override
+  MyCustomFormStatePrimaryRequest createState() {
+    return MyCustomFormStatePrimaryRequest();
+  }
+}
+class MyCustomFormStatePrimaryRequest extends State<MyCustomFormPrimaryRequest> {
+  // Create a global key that uniquely identifies the Form widget
+  // and allows validation of the form.
+  //
+  // Note: This is a GlobalKey<FormState>,
+  // not a GlobalKey<MyCustomFormState>.
+  final _formKey = GlobalKey<FormState>();
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  final severityController = TextEditingController();
+  final itemController = TextEditingController();
 
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is disposed.
+    emailController.dispose();
+    passwordController.dispose();
+    severityController.dispose();
+    itemController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // Build a Form widget using the _formKey created above.
+    return Form(
+      key: _formKey,
+      child: ListView(
+        children: <Widget>[
+          Text('Level of Severity', textAlign: TextAlign.center, textScaleFactor: 2),
+          TextFormField(
+            controller: severityController,
+            validator: (value) {
+              if (value.isEmpty) {
+                return 'Please enter  some text';
+              }
+              return null;
+            },
+          ),
+          Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16.0)
+          ),
+          Text('Item', textAlign: TextAlign.center, textScaleFactor: 2,),
+          TextFormField(
+            controller: itemController,
+            validator: (value) {
+              if (value.isEmpty) {
+                return 'Please enter some text';
+              }
+              return null;
+            },
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16.0),
+            child: RaisedButton(
+              onPressed: () {
+                // Validate returns true if the form is valid, or false
+                // otherwise.
+                if (_formKey.currentState.validate()) {
+                  _makePostRequest() async {
+                    // set up POST request arguments
+                    String url = 'https://hackpsu-fall2019.herokuapp.com/rest-auth/login/';
+                    Map<String, String> headers = {"Content-type": "application/json"};
+                    String email = emailController.text;
+                    String password = passwordController.text;
+                    String severity = severityController.text;
+                    String item = itemController.text;
+                    print(email);
+                    print(password);
+                    String json = '{"email": "$email", "password": "$password", "severity" : "$severity", "item" : "$item"}';
+                    Response response = await post(url, headers: headers, body: json);
+                    int statusCode = response.statusCode;
+                    String body = response.body;
+                    print(body);
+                    print(statusCode);
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => RequestDetails()));
+                  }
+                  Scaffold.of(context)
+                      .showSnackBar(SnackBar(content: Text('Processing Data')));
+                  _makePostRequest();
+                }
+              },
+              child: Text('Submit'),
+            ),
+          ),
+        ],
       ),
     );
-
   }
 }
 
-class RequestDetails extends StatelessWidget{
+class RequestDetails extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Request Details Page',
+      title: 'I Need Help',
       home: Scaffold(
-          appBar: AppBar(
-            title: Text('Details'),
+        appBar: AppBar(
+          title: Text('I Neep Help'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              RaisedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => PrimaryRequest()),
+                  );
+                },
+                textColor: Colors.white,
+                padding: const EdgeInsets.all(0.0),
+                child: Container(
+                  decoration: const BoxDecoration(
+                    gradient: LinearGradient(
+                      colors: <Color>[
+                        Color(0xFF0D47A1),
+                        Color(0xFF1976D2),
+                        Color(0xFF42A5F5),
+                      ],
+                    ),
+                  ),
+                  padding: const EdgeInsets.all(10.0),
+                  child: const Text(
+                      'Request',
+                      style: TextStyle(fontSize: 20)
+                  ),
+                ),
+              ),
+              const SizedBox(height: 30),
+              RaisedButton(
+                onPressed: () {},
+                textColor: Colors.white,
+                padding: const EdgeInsets.all(0.0),
+                child: Container(
+                  decoration: const BoxDecoration(
+                    gradient: LinearGradient(
+                      colors: <Color>[
+                        Color(0xFF0D47A1),
+                        Color(0xFF1976D2),
+                        Color(0xFF42A5F5),
+                      ],
+                    ),
+                  ),
+                  padding: const EdgeInsets.all(10.0),
+                  child: const Text(
+                      'Send',
+                      style: TextStyle(fontSize: 20)
+                  ),
+                ),
+              ),
+              const SizedBox(height: 30),
+              RaisedButton(
+                onPressed: () {},
+                textColor: Colors.white,
+                padding: const EdgeInsets.all(0.0),
+                child: Container(
+                  decoration: const BoxDecoration(
+                    gradient: LinearGradient(
+                      colors: <Color>[
+                        Color(0xFF0D47A1),
+                        Color(0xFF1976D2),
+                        Color(0xFF42A5F5),
+                      ],
+                    ),
+                  ),
+                  padding: const EdgeInsets.all(10.0),
+                  child: const Text(
+                      'Profile',
+                      style: TextStyle(fontSize: 20)
+                  ),
+                ),
+              ),
+            ],
           ),
-          body: ListView(
-            children: ListTile.divideTiles(
-              context: context,
-              tiles: [
-                Container(
-                    color: Colors.grey[200],
-                    child: ListTile(
-                        title: Center(
-                          child: Text('Go Back', style: TextStyle(fontWeight: FontWeight.bold)),
-                        ),
-                        onTap: (){
-                          Navigator.pop(context);
-                        }
-                    )
-                ),
-                Column(
-                  children: <Widget>[
-                    ListTile(
-                      title: Text('Severity of Situation', textScaleFactor: 1.1, textAlign: TextAlign.center,),
-                    ),
-                    ListTile(
-                      title: Text('High'),
-                    ),
-                    ListTile(
-                      title: Text('Medium'),
-                    ),
-                    ListTile(
-                      title: Text('Low'),
-                    ),
-                  ],
-                ),
-                Column(
-                  children: <Widget>[
-                    ListTile(
-                      title: Text('Items', textScaleFactor: 1.1, textAlign: TextAlign.center,),
-                    ),
-                    ListTile(
-                      title: Text('Food/water'),
-                    ),
-                    ListTile(
-                      title: Text('Clothing'),
-                    ),
-                    ListTile(
-                      title: Text('Shelter'),
-                    ),
-                    ListTile(
-                      title: Text('Toiletries'),
-                    ),
-                    Container(
-                        color: Colors.grey[200],
-                        child: ListTile(
-                            title: Center(
-                              child: Text('Next', style: TextStyle(fontWeight: FontWeight.bold)),
-                            ),
-                            onTap: (){
-                              Navigator.pop(context);
-                            }
-                        )
-                    ),
-                  ],
-                )
-              ],
-            ).toList(),
-          )
-
+        ),
       ),
     );
-
   }
 }
+
+// Create a Form widget.
