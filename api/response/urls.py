0c6211b7366b20cf8146b4493da49aa7a2f7d2bf
Alexander Skvortsov <askvortsov@youthneuro.org>
2019-11-03 12:39:46 -0500

    Fixed more bugs

--- api/response/urls.py
+++ api/response/urls.py
@@ -1,12 +1,12 @@
 from rest_framework.routers import DefaultRouter
-from .views import HelpRequestViewSet, HelpRequestCreate, HelpRequestDestroy
+from .views import HelpResponseViewSet, HelpResponseCreate, HelpResponseDestroy
 from django.urls import path
 
 
 router = DefaultRouter()
-router.register(r'requests', HelpResponseViewSet, basename='user')
+router.register(r'response', HelpResponseViewSet, basename='user')
 urlpatterns = router.urls
 urlpatterns += [
-    path('requests/create/', HelpRequestCreate),
-    path('requests/destroy/', HelpRequestDestroy),
+    path('response/create/', HelpResponseCreate),
+    path('response/destroy/', HelpResponseDestroy),
 ]
